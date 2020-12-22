0b59428c5d37aa0b7dc769f487c8999eb240ae48
Remmington Pascone <remmingtonpearce@gmail.com>
2020-12-22 13:38:06 -0800

    I guess it helps if you link your css to your html

--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -3,13 +3,15 @@ var startBtn = document.getElementById("start");
 var questionsElement = document.getElementById("questions");
 
 //call startQuiz function on button click 
-startBtn.onclick = satrtQuiz; 
+startBtn.onclick = startQuiz; 
 
 //function for kicking off the quiz
 function startQuiz() { 
-    var startScreen = document.querySelector("#start-screem"); 
+    var startScreen = document.querySelector("#start-screen"); 
     startScreen.setAttribute("class", "hide");
 
     //Unhide the questions
-    questionsElement.removeAttribute("class")
+    questionsElement.removeAttribute("class");
+
+    
 }
